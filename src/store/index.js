eca2b30805917c2066779b2e5bee772b567b30a1
小陈菌 <chen_studio@qq.com>
2021-01-08 00:49:38 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    
+    // Normal BEGIN ----------------------------------------
+    resList: [
+      {
+        title: "【ASMR】被堕天使玩弄耳朵 / 足部按摩",
+        hash: "A13E78B1829504250220A5D927FDCC545597302B",
+        murl:
+          "magnet:?xt=urn:btih:a13e78b1829504250220a5d927fdcc545597302b&dn=%e3%80%90ASMR%e3%80%91%e5%a0%95%e5%a4%a9%e4%bd%bf%e7%9a%84%e6%88%98%e4%ba%89%e8%b7%b5%e8%b8%8f.zip&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2fexplodie.org%3a6969%2fannounce&tr=http%3a%2f%2ft.nyaatracker.com%3a80%2fannounce&tr=http%3a%2f%2fsukebei.tracker.wf%3a8888%2fannounce&tr=http%3a%2f%2fnyaa.tracker.wf%3a7777%2fannounce",
+      },
+    ],
+    // Normal END ------------------------------------------
+
+    // R18 BEGIN -------------------------------------------
+    resListR18: [
+      {
+        title: "猫窝首翻 / 纱雾本子",
+        hash: "AE3223CE4AEE77496D01A314AD2BB746B8E0FD61",
+        murl:
+          "magnet:?xt=urn:btih:ae3223ce4aee77496d01a314ad2bb746b8e0fd61&dn=%e3%80%90%e7%8c%ab%e7%aa%9d%e9%a6%96%e7%bf%bb%e3%80%91%e7%ba%b1%e9%9b%be%e6%9c%ac%e5%ad%90.zip&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2fexplodie.org%3a6969%2fannounce&tr=http%3a%2f%2ft.nyaatracker.com%3a80%2fannounce&tr=http%3a%2f%2fsukebei.tracker.wf%3a8888%2fannounce&tr=http%3a%2f%2fnyaa.tracker.wf%3a7777%2fannounce",
+      },
+    ],
+    // R18 END ---------------------------------------------
+
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         </p>
       </div>
       <br />
-      <ResTable :resData="resList"/>
+      <ResTable :resData="this.$store.state.resList"/>
     </template>
   </div>
 </template>
@@ -22,18 +22,6 @@ export default {
   name: "Home",
   components: {
     ResTable,
-  },
-  data() {
-    return {
-      resList: [
-        {
-          title: "【ASMR】被堕天使玩弄耳朵 / 足部按摩",
-          hash: "A13E78B1829504250220A5D927FDCC545597302B",
-          murl:
-            "magnet:?xt=urn:btih:a13e78b1829504250220a5d927fdcc545597302b&dn=%e3%80%90ASMR%e3%80%91%e5%a0%95%e5%a4%a9%e4%bd%bf%e7%9a%84%e6%88%98%e4%ba%89%e8%b7%b5%e8%b8%8f.zip&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2fexplodie.org%3a6969%2fannounce&tr=http%3a%2f%2ft.nyaatracker.com%3a80%2fannounce&tr=http%3a%2f%2fsukebei.tracker.wf%3a8888%2fannounce&tr=http%3a%2f%2fnyaa.tracker.wf%3a7777%2fannounce",
-        },
-      ],
-    };
   },
 };
 </script>
