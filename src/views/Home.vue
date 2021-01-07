74d5de4c663f434341813403653f2e03cd546eb9
小陈菌 <chen_studio@qq.com>
2021-01-07 14:27:04 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,9 +19,9 @@
         "
       >
         <el-table-column prop="title" label="Title"> </el-table-column>
-        <el-table-column prop="hash" label="Hash" width="380">
+        <el-table-column prop="hash" label="Hash">
         </el-table-column>
-        <el-table-column label="MagnetURL" width="120">
+        <el-table-column label="MagnetURL" width="110">
           <template slot-scope="scope">
             <el-link :href="scope.row.murl" type="primary">Download</el-link>
           </template>
